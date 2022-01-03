aba30877d7bddb1842d8156c74f8efa9037579ca
duc <antran26062001@gmail.com>
2022-01-03 16:07:01 +0700

    first commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Moi ban nhap ten: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello {0}" ,name + "!");
+            Console.WriteLine(" Welcome to NTQ Solution.");
+        }
+    }
+}
